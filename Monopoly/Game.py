3a680389cd30afd73dbe4839f43ede942ef2b718
SeungwooDY <sdyoon0701@gmail.com>
2025-10-26 17:21:55 -0400

    created project structure

---
+++ Monopoly/Game.py
@@ -0,0 +1,24 @@
+class Game:
+    def __init__(self, player_names: list[str]):
+        return
+
+    def start(self):
+        return
+
+    def next_turn(self):
+        return
+
+    def roll_dice(self) -> tuple[int, int]:
+        return
+
+    def move_player(self, player, steps: int):
+        return
+
+    def handle_space(self, player):
+        return
+
+    def check_bankruptcy(self, player):
+        return
+
+    def end_game(self):
+        return
