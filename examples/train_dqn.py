441613315fcdfb7317b7070b9a35275d0da6b5c0
Connor <164671442+ConnorC3@users.noreply.github.com>
2026-03-23 17:41:30 -0400

    Change log to every 100 episodes

--- examples/train_dqn.py
+++ examples/train_dqn.py
@@ -244,5 +244,5 @@ if __name__ == '__main__':
         training_stage=2,
         load_path='checkpoints/dqn_phase4b.pt',
         save_path='checkpoints/dqn_phase4c.pt',
-        log_every=1,
+        log_every=100,
     )
