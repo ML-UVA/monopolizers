ae4294e9809c27dd0a3b8ca2314666afca71596d
Connor <164671442+ConnorC3@users.noreply.github.com>
2026-03-23 22:30:00 -0400

    Lower learning rate

--- examples/train_dqn.py
+++ examples/train_dqn.py
@@ -143,7 +143,7 @@ def train(
         n_actions=n_actions,
         device=device,
         gamma=0.99,
-        lr=1e-4, # lower lr for fine-tuning
+        lr=1e-5, # lower lr for fine-tuning
         epsilon_decay=2_000_000,
         buffer_capacity=200_000,
         batch_size=128,
